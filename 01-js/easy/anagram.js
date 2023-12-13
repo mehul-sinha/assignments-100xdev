c6ae5ae7c7edd3f2e172004970f70395fdeafa21
mehul-sinha <mehulsinha2001@gmail.com>
2023-12-13 17:07:25 +0530

    anagram test

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -6,6 +6,20 @@
 
 function isAnagram(str1, str2) {
 
+  if(str1.length != str2.length){
+    return false;
+  }
+
+  let inp1 = str1.toLowerCase().split('').sort();
+  let inp2 = str2.toLowerCase().split('').sort();
+
+  for(let i = 0; i < inp1.length; i++){
+    if(inp1[i] != inp2[i]){
+      return false;
+    }
+  }
+  return true;
+
 }
 
 module.exports = isAnagram;
