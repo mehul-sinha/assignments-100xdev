12865722f8ed66f2988653d40b445bf06b0668fc
mehul-sinha <mehulsinha2001@gmail.com>
2024-01-11 22:08:28 +0530

    middleware solution

--- week-3/01-middlewares/01-requestcount.js
+++ week-3/01-middlewares/01-requestcount.js
@@ -3,16 +3,23 @@ const assert = require('assert');
 const express = require('express');
 
 const app = express();
-let requestCount = 0;
 
 // You have been given an express server which has a few endpoints.
 // Your task is to create a global middleware (app.use) which will
 // maintain a count of the number of requests made to the server in the global
 // requestCount variable
 
+let requestCount = 0;
+function requestCountMiddleware(req, res, next){
+  requestCount+=1;
+  next();
+}
+
+app.use(requestCountMiddleware);
+
 app.get('/user', function(req, res) {
   res.status(200).json({ name: 'john' });
-});
+}); 
 
 app.post('/user', function(req, res) {
   res.status(200).json({ msg: 'created dummy user' });
--- week-3/01-middlewares/02-ratelimitter.js
+++ week-3/01-middlewares/02-ratelimitter.js
@@ -16,6 +16,23 @@ setInterval(() => {
     numberOfRequestsForUser = {};
 }, 1000)
 
+app.use(function(req, res, next){
+  const userID = req.headers["user-id"];
+  if(numberOfRequestsForUser.userID){
+    numberOfRequestsForUser.userID += 1;
+    if(numberOfRequestsForUser.userID > 5){
+      res.status(404).send("Too many requests")
+    }
+    else{
+      next();
+    }
+  }
+  else{
+    numberOfRequestsForUser.userID = 1;
+    next();
+  }
+})
+
 app.get('/user', function(req, res) {
   res.status(200).json({ name: 'john' });
 });
